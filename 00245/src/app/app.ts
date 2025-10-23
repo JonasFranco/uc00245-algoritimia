76f1e369931eb74e2e56e6336c35ead6b1357dfc
JonasFranco <jonasfrancobn@gmail.com>
2025-10-23 22:35:22 +0100

    1 comit

---
+++ 00245/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Main],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  //protected readonly title = signal('00245');
+}
